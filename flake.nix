19439aa3737be233711b5b30a51d7a65e8e3332a
AsciiMoth <ascii@moth.contact>
2025-09-14 20:08:12 +0400

    chore: bootstrap project from template

---
+++ flake.nix
@@ -0,0 +1,76 @@
+{
+  description = "Colorit";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pre-commit-hooks,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      release = pkgs.writeShellScriptBin "release" (builtins.readFile ./ci/release);
+
+      checks = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            commitizen.enable = true;
+            typos.enable = true;
+            typos-commit = {
+              enable = true;
+              description = "Find typos in commit message";
+              entry = let script = pkgs.writeShellScript "typos-commit" ''
+                typos "$1"
+              ''; in builtins.toString script;
+              stages = [ "commit-msg" ];
+            };
+            govet.enable = true;
+            gofmt.enable = true;
+            golangci-lint.enable = true;
+            gotidy = {
+              enable = true;
+              description = "Makes sure go.mod matches the source code";
+              entry = let script = pkgs.writeShellScript "gotidyhook" ''
+                go mod tidy -v
+                if [ -f "go.mod" ]; then
+                  git add go.mod
+                fi
+                if [ -f "go.sum" ]; then
+                  git add go.sum
+                fi
+              ''; in builtins.toString script;
+              stages = [ "pre-commit" ];
+            };
+          };
+        };
+      };
+    in {
+      devShell = pkgs.mkShell {
+        inherit (checks.pre-commit-check) shellHook;
+        buildInputs = with pkgs; [
+          go
+          golangci-lint
+          commitizen
+          goreleaser
+          git-cliff
+          govulncheck
+
+          typos
+
+          yq
+          jq
+
+          release
+        ];
+      };
+    });
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/asciimoth/colorit
+
+go 1.24.6
